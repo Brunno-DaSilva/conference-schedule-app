9cab57ddf7317ed627345b4a58e3346ffe4b192f
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-07 15:24:32 -0600

    Created project base folder structure

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./footer.styles.scss";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Footer component</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
