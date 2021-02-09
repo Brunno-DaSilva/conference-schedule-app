1eb92e036c9e50fb375dafd97a32ca8ad1ec29a3
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-09 09:57:41 -0600

    Added font-awesome and change the hero desc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faCalendarAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
 import "./header.styles.scss";
 
 function Header(props) {
@@ -9,9 +15,9 @@ function Header(props) {
       <Navbar />
       <div className="Header__hero">
         <div className="hero__text">
-          <p>Digital Design</p>
+          <p>Digital Coding</p>
           <p> Conference </p>
-          <p>2019 NYC</p>
+          <p>2022 Dallas</p>
         </div>
         <div className="hero__button">
           <button type="button">Add to Your Calendar</button>
@@ -19,26 +25,41 @@ function Header(props) {
       </div>
       <div className="Header__info">
         <div className="info__location">
-          <p>City Hall, New York City</p>
+          <p>
+            {" "}
+            <FontAwesomeIcon
+              className="info-icons active"
+              icon={faMapMarkerAlt}
+            />
+            City Hall, New York City
+          </p>
         </div>
         <div className="info__time">
-          <p>12-15 MARCH 2022</p>
+          <p>
+            <span>
+              <FontAwesomeIcon
+                className="info-icons active"
+                icon={faCalendarAlt}
+              />
+            </span>{" "}
+            12-15 MARCH 2022
+          </p>
         </div>
         <div className="info__countdown">
           <div className="info__countdown--time">
-            <p className="zero">00</p>
+            <p className="zeros">00</p>
             <p>Days</p>
           </div>
           <div className="info__countdown--time">
-            <p className="zero">00</p>
+            <p className="zeros">00</p>
             <p>Hours</p>
           </div>
           <div className="info__countdown--time">
-            <p className="zero">00</p>
+            <p className="zeros">00</p>
             <p>Minutes</p>
           </div>
           <div className="info__countdown--time">
-            <p>00</p>
+            <p className="zeros">00</p>
             <p>Seconds</p>
           </div>
         </div>
